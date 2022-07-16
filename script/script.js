056097d12b2a8b3ed99d1ccf3dac69cd4d363677
Danya Pavlov <fRo0neeee@gmail.com>
2022-07-16 23:40:22 +0300

    feat: слайдеры, tablet и прочие так же фиксы

--- script/script.js
+++ script/script.js
@@ -1,6 +1,47 @@
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__close-button");
+const sectionNews = document.querySelector(".news-main");
+const sectionNewsMain = document.querySelector(".news-main__all");
+const templateButtonNewsMain = document.querySelector("#button__tablet").content;
+const templateButtonDesktop = document.querySelector("#button__desktop").content;
+
+function checkWidthElement() {
+  return window.innerWidth;
+}
+
+function replacedButtonNews() {
+  const widthSection = checkWidthElement(sectionNews);
+  if(widthSection < 1024) {
+    checkTabletButton() == null ? sectionNewsMain.replaceChild(createButtonTablet(), checkDesktopButton()) : false;
+    return;
+  }
+  checkDesktopButton() == null ? sectionNewsMain.replaceChild(createButtonDesktop(), checkTabletButton()) : false;
+}
+
+function checkTabletButton() {
+  return sectionNewsMain.querySelector('.news-main__tablet-container');
+}
+
+function checkDesktopButton() {
+  return sectionNewsMain.querySelector('.button_news');
+}
+
+function createButtonTablet() {
+  return templateButtonNewsMain.querySelector('.news-main__tablet-container').cloneNode(true);
+}
+
+function createButtonDesktop() {
+  return templateButtonDesktop.cloneNode(true);
+}
 
 closeButton.addEventListener("click", () => {
   popup.classList.remove("popup_opened");
 });
+
+
+window.addEventListener('resize', function(event){
+  replacedButtonNews();
+});
+
+replacedButtonNews();
+
--- script/slider.js
+++ script/slider.js
@@ -1,124 +1,204 @@
-const sliderContainer = document.querySelector('.slider__items');
-const itemSliderWidth = document.querySelector('.item_slider').offsetWidth;
-const itemSlider = document.querySelectorAll('.item_slider');
-const arrowLeft = document.querySelector('.arrow_left');
-const arrowRight = document.querySelector('.arrow_right');
-const sliderIndicators = document.querySelector('.slider__indicators');
-const sliderIndicator = document.querySelector('.slider__indicator');
-const defaultTransform = 30;
+// все стрелки в пейдже
+const arrowLeftAll = document.querySelectorAll('.arrow_left');
+const arrowRightAll = document.querySelectorAll('.arrow_right');
+// индикаторы в пейдже
+const sliderIndicatorsAll = document.querySelectorAll('.slider__indicator');
+// дефолтный шаг для индикатора и система координат.
 const defautlTransformIndicator = 15;
 let y = 0;
 let x = 0;
 let isDrawing = false;
 
-function swiping(xOne, xTwo) {
-  const valueTransform = getTransformValue(sliderIndicator);
+/**
+ * метод обработки слайдера, проверка разницы координат, вызов экшинов по кнопкам, установка значений
+ * @param {int} xOne ось и координаты где произошло события захвата ползунка
+ * @param {int} xTwo ось и координаты куда произошел сдвиг
+ * @param {object} sliderContainer объект типа элемент, контейнер где лежат наши слайды
+ * @param {object} indicatorContainer объект типа элемент, ползунок для слайдов
+ * @param {NodeList} itemSlider коллекция слайдов
+ * @param {object} buttonLeft
+ * @param {object} buttonRight
+ * @returns
+ */
+function swiping(xOne,xTwo,sliderContainer,indicatorContainer,itemSlider,buttonLeft,buttonRight) {
+  const valueTransform = getTransformValue(indicatorContainer);
   if(xOne < xTwo) {
-    actionButtonRight();
-    swipingRight(+valueTransform + defautlTransformIndicator);
+    actionButtonRight(buttonRight,sliderContainer,indicatorContainer,itemSlider,buttonLeft);
+    swipingRight(+valueTransform + defautlTransformIndicator,indicatorContainer,itemSlider);
     return;
   }
-  actionButtonLeft();
-  swipingLeft(+valueTransform - defautlTransformIndicator);
+  actionButtonLeft(buttonLeft,sliderContainer,indicatorContainer,buttonRight,itemSlider);
+  swipingLeft(+valueTransform - defautlTransformIndicator,indicatorContainer);
 }
-
-function swipingLeft(value) {
-  value >= 0 ? setTransformValueIndicator(value) : false;
+// свайп ползунка влево, проверка значения translateX. Вызов сеттера
+function swipingLeft(value,sliderIndicator) {
+  value >= 0 ? setTransformValueIndicator(value,sliderIndicator) : false;
 }
-
-function swipingRight(value) {
+// свайп ползунка вправо, проверка максов и значения translateX. Вызов сеттера
+function swipingRight(value,sliderIndicator,itemSlider) {
   const maxTransformValue = itemSlider.length * defautlTransformIndicator - defautlTransformIndicator;
   if(getTransformValue(sliderIndicator) < maxTransformValue) {
-    setTransformValueIndicator(value);
+    setTransformValueIndicator(value,sliderIndicator);
   }
 }
-
-function disableButtonLeft () {
-  const valueTransform = getTransformValue(sliderContainer);
+// проверка и установка доступности кнопок, в зависимости от значения translateX.
+function disableButtonLeft (button,container) {
+  const valueTransform = getTransformValue(container);
   if (valueTransform > 0) {
-    arrowLeft.disabled = false;
+    button.disabled = false;
     return;
   }
-  arrowLeft.disabled = true;
+  button.disabled = true;
 }
 
-function disableButtonRight(value) {
+function disableButtonRight(value,button,itemSliderWidth,itemSlider) {
   const maxTransform = itemSlider.length * itemSliderWidth - itemSliderWidth;
   if(value >= maxTransform) {
-    arrowRight.disabled = true;
+    button.disabled = true;
     return;
   }
-  arrowRight.disabled = false;
+  button.disabled = false;
 }
-
+// метод получения цифрового занчения трансформа
 function getTransformValue(container) {
   return container.style.transform.replace(/[^\d.]/g, '');
 }
-
+// метод установки значения для translate по оси Х для контейнера слайдеров
 function setTransformValue(container, value) {
   container.style.transform = `translateX(-${value}px)`;
 }
-
-function setTransformValueIndicator(value) {
+// метод установки значения для translate по оси Х для ползунка слайдеров
+function setTransformValueIndicator(value,sliderIndicator) {
   sliderIndicator.style.transform = `translateX(${value}px)`;
 }
+/**
+ * метод обработки события при клике на кнопку влево. установка значений, проверка кнопок на доступность.
+ * @param {object} button сама кнопка на которую сделали click
+ * @param {object} container объект типа элемент, контейнер где лежат наши слайды
+ * @param {object} parentContainer объект типа элемент, ползунок для слайдов
+ * @param {object} buttonRight
+ * @param {NodeList} itemSlider коллекция слайдов
+ * @returns
+ */
+function actionButtonLeft(button,container,parentContainer,buttonRight,itemSlider) {
+  const valueTransform = getTransformValue(container);
+  const valueTransformIndicator = getTransformValue(parentContainer);
+  const itemSliderWidth = calculateWidthElement(container);
 
-function actionButtonLeft() {
-  const valueTransform = getTransformValue(sliderContainer);
-  const valueTransformIndicator = getTransformValue(sliderIndicator);
-  if(valueTransform >= defaultTransform) {
-    setTransformValue(sliderContainer, +valueTransform - +itemSliderWidth);
-    swipingLeft(+valueTransformIndicator - defautlTransformIndicator);
-    disableButtonRight(+valueTransform - +defaultTransform);
-    disableButtonLeft();
+  if(valueTransform >= itemSliderWidth) {
+    setTransformValue(container, +valueTransform - +itemSliderWidth);
+    swipingLeft(+valueTransformIndicator - defautlTransformIndicator,parentContainer);
+    disableButtonRight(+valueTransform - +itemSliderWidth,buttonRight,itemSliderWidth,itemSlider);
+    disableButtonLeft(button,container);
     return;
   }
-  setTransformValue(sliderContainer, '');
-  swipingLeft(+valueTransformIndicator - defautlTransformIndicator);
-  disableButtonLeft();
+  setTransformValue(container, '');
+  swipingLeft(+valueTransformIndicator - defautlTransformIndicator,parentContainer);
+  disableButtonLeft(button,container);
 }
+/**
+ * метод обработки события при клике на кнопку вправо. установка значений, проверка кнопок на доступность.
+ * @param {object} button сама кнопка на которую сделали click
+ * @param {object} container объект типа элемент, контейнер где лежат наши слайды
+ * @param {object} parentContainer объект типа элемент, ползунок для слайдов
+ * @param {NodeList} itemSlider коллекция слайдов
+ * @param {object} buttonLeft
+ * @returns
+ */
+function actionButtonRight(button,container,parentContainer,itemSlider,buttonLeft) {
+  const valueTransform = getTransformValue(container);
+  const valueTransformIndicator = getTransformValue(parentContainer);
+  const itemSliderWidth = calculateWidthElement(container);
 
-function actionButtonRight() {
-  const valueTransform = getTransformValue(sliderContainer);
-  const valueTransformIndicator = getTransformValue(sliderIndicator);
   const newTransform = +valueTransform + +itemSliderWidth;
   const checkMaxTransform = itemSlider.length * itemSliderWidth - itemSliderWidth;
   if(newTransform > checkMaxTransform) {
     return false;
   }
   if(valueTransform >= itemSliderWidth) {
-    swipingRight(+valueTransformIndicator + defautlTransformIndicator);
-    setTransformValue(sliderContainer, newTransform);
-    disableButtonRight(newTransform);
+    swipingRight(+valueTransformIndicator + defautlTransformIndicator,parentContainer,itemSlider);
+    setTransformValue(container, newTransform);
+    disableButtonRight(newTransform,button,itemSliderWidth,itemSlider);
     return;
   }
-  swipingRight(+valueTransformIndicator + defautlTransformIndicator);
-  setTransformValue(sliderContainer, itemSliderWidth);
-  disableButtonLeft();
+  swipingRight(+valueTransformIndicator + defautlTransformIndicator,parentContainer,itemSlider);
+  setTransformValue(container, itemSliderWidth);
+  disableButtonLeft(buttonLeft,container);
+}
+// геттеры на элементы
+function prepareSliderContainer(element) {
+  return element.querySelector('.slider__items');
 }
 
-arrowLeft.addEventListener('click', () => {
-  actionButtonLeft();
-});
+function calculateWidthElement(element) {
+  return element.querySelector('.item_slider').offsetWidth;
+}
 
-arrowRight.addEventListener('click', () => {
-  actionButtonRight();
-});
+function getParentContainer(element) {
+  return element.closest('.slider__container');
+}
 
-sliderIndicator.addEventListener('mousedown', e => {
-  isDrawing = true;
-  x = e.offsetX;
-});
+function getSliderIndicator(element) {
+  return element.querySelector('.slider__indicator');
+}
 
-sliderIndicator.addEventListener('mousemove', e => {
+function getItemSliderAll(element) {
+  return element.querySelectorAll('.item_slider');
+}
+
+function getButtonLeft(element) {
+  return element.querySelector('.arrow_left');
+}
+
+function getButtonRight(element) {
+  return element.querySelector('.arrow_right')
+}
+// слушатель событий на кнопку ВЛЕВО, работа сразу с неограниченным количеством слайдеров.
+arrowLeftAll.forEach( el => {
+  const parentContainer = getParentContainer(el);
+  const sliderContainer = prepareSliderContainer(parentContainer);
+  const indicatorContainer = getSliderIndicator(parentContainer);
+  const itemSlider = getItemSliderAll(parentContainer);
+  const buttonRight = getButtonRight(parentContainer);
+
+  disableButtonLeft(el,sliderContainer);
+  el.addEventListener('click', () => {
+    actionButtonLeft(el,sliderContainer,indicatorContainer,buttonRight,itemSlider);
+  });
+});
+// слушатель событий на кнопку ВПРАВО, работа сразу с неограниченным количеством слайдеров.
+arrowRightAll.forEach( el => {
+  const parentContainer = getParentContainer(el);
+  const sliderContainer = prepareSliderContainer(parentContainer);
+  const indicatorContainer = getSliderIndicator(parentContainer);
+  const itemSlider = getItemSliderAll(parentContainer);
+  const buttonLeft = getButtonLeft(parentContainer);
+
+  el.addEventListener('click', () => {
+    actionButtonRight(el,sliderContainer,indicatorContainer,itemSlider,buttonLeft);
+  });
+});
+// слушатель событий на индикатор, работа сразу с неограниченным количеством слайдеров.
+sliderIndicatorsAll.forEach( el => {
+  const parentContainer = getParentContainer(el);
+  const sliderContainer = prepareSliderContainer(parentContainer);
+  const indicatorContainer = getSliderIndicator(parentContainer);
+  const itemSlider = getItemSliderAll(parentContainer);
+  const buttonLeft = getButtonLeft(parentContainer);
+  const buttonRight = getButtonRight(parentContainer);
+  el.addEventListener('mousedown', e => {
+    isDrawing = true;
+    x = e.offsetX;
+  });
+  el.addEventListener('mousemove', e => {
     if(isDrawing) {
-      swiping(x, e.offsetX);
+      swiping(x, e.offsetX,sliderContainer,indicatorContainer,itemSlider,buttonLeft,buttonRight);
       isDrawing = false;
     }
+  });
+  el.addEventListener('mouseup', () => {
+    isDrawing = false;
+  });
 });
 
-sliderIndicator.addEventListener('mouseup', () => {
-  isDrawing = false;
-});
 
-disableButtonLeft();
