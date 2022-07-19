0dd9cb2d5acf61e5f5c636927b2a3021bc92e4fe
Danya Pavlov <fRo0neeee@gmail.com>
2022-07-19 21:44:26 +0300

    feat: sliders, news, materials

--- script/script.js
+++ script/script.js
@@ -1,37 +1,120 @@
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__close-button");
 const sectionNews = document.querySelector(".news-main");
-const sectionNewsMain = document.querySelector(".news-main__all");
+const sliderNewsContainer = sectionNews.querySelector('.slider__items');
+const sectionNewsMain = document.querySelectorAll(".news-main__all");
+const journalMainBlock = document.querySelector('.journal-projito');
+const sliderJournalContainer = journalMainBlock.querySelector('.slider__items');
+const sectionReaderAll = document.querySelectorAll('.journal-projito__card_reader');
 const templateButtonNewsMain = document.querySelector("#button__tablet").content;
 const templateButtonDesktop = document.querySelector("#button__desktop").content;
+const templateNewsMainBlock = document.querySelector("#template-news").content;
+const templateProjitoCard = document.querySelector('#template-projito').content;
+const templateProjitoReader = document.querySelector('#template-reader').content;
+const limitIndex = 6;
+const limitMobile = 3;
 
 function checkWidthElement() {
   return window.innerWidth;
 }
 
-function replacedButtonNews() {
-  const widthSection = checkWidthElement(sectionNews);
+function replacedButtonNews(container,parentContainer) {
+  const widthSection = checkWidthElement();
   if(widthSection < 1024) {
-    checkTabletButton() == null ? sectionNewsMain.replaceChild(createButtonTablet(), checkDesktopButton()) : false;
+    checkTabletButton(container) == null ? container.replaceChild(createButtonTablet(parentContainer), checkDesktopButton(container)) : false;
     return;
   }
-  checkDesktopButton() == null ? sectionNewsMain.replaceChild(createButtonDesktop(), checkTabletButton()) : false;
+  checkDesktopButton(container) == null ? container.replaceChild(createButtonDesktop(parentContainer), checkTabletButton(container)) : false;
 }
 
-function checkTabletButton() {
-  return sectionNewsMain.querySelector('.news-main__tablet-container');
+function checkTabletButton(container) {
+  return container.querySelector('.news-main__tablet-container');
 }
 
-function checkDesktopButton() {
-  return sectionNewsMain.querySelector('.button_news');
+function checkDesktopButton(container) {
+  return container.querySelector('.button_news');
 }
 
-function createButtonTablet() {
-  return templateButtonNewsMain.querySelector('.news-main__tablet-container').cloneNode(true);
+function createButtonTablet(parentContainer) {
+  const newButton = templateButtonNewsMain.querySelector('.news-main__tablet-container').cloneNode(true);
+  switch (parentContainer.classList.value) {
+    case 'news-main':
+      newButton.querySelector('.news-main__text-button').textContent = 'Ко всем новостям';
+      break;
+    case 'journal-projito':
+      newButton.querySelector('.news-main__text-button').textContent = 'Посмотреть всю подборку';
+      break;
+  }
+  return newButton;
 }
 
-function createButtonDesktop() {
-  return templateButtonDesktop.cloneNode(true);
+function createButtonDesktop(parentContainer) {
+  const newButton = templateButtonDesktop.cloneNode(true);
+  switch (parentContainer.classList.value) {
+    case 'news-main':
+      newButton.querySelector('.button_news').textContent = 'Ко всем новостям';
+      break;
+    case 'journal-projito':
+      newButton.querySelector('.button_news').textContent = 'Посмотреть всю подборку';
+      break;
+  }
+  return newButton;
+}
+
+function createNewsCard(item) {
+  const sliderNews = templateNewsMainBlock.querySelector('.item_slider').cloneNode(true);
+  const newsDate = sliderNews.querySelector('.item__date');
+  const newsTag = sliderNews.querySelector('.item__tag');
+  const newsTitle = sliderNews.querySelector('.item__title');
+  const newsImage = sliderNews.querySelector('.item__image');
+  newsDate.textContent = item.date;
+  newsTag.textContent = item.tag;
+  newsTitle.textContent = item.title;
+  newsImage.src = item.image;
+  newsImage.alt = item.alt;
+  return sliderNews;
+
+}
+function createReaderCard (item) {
+  const sliderMaterial = templateProjitoReader.querySelector('.journal-projito__card').cloneNode(true);
+  const materialTitle = sliderMaterial.querySelector('.journal-projito__text-title');
+  const materialPhoto = sliderMaterial.querySelector('.journal-projito__reader-photo');
+  const materialText = sliderMaterial.querySelector('.journal-projito__text-quote');
+  const mateirialQuote = sliderMaterial.querySelector('.journal-projito__text-quote_reader');
+  materialPhoto.src = item.image;
+  materialPhoto.alt = item.alt;
+  materialTitle.textContent = item.title;
+  mateirialQuote.textContent = item.about;
+  materialText.textContent = item.quote;
+  return sliderMaterial;
+}
+function createMaterialsCard(item) {
+  if(item.reader) {
+    return createReaderCard(item);
+  }
+  const sliderMaterial = templateProjitoCard.querySelector('.journal-projito__card').cloneNode(true);
+  const materialTitle = sliderMaterial.querySelector('.journal-projito__text-title');
+  const materialSubtitle = sliderMaterial.querySelector('.journal-projito__text-subtitle');
+  const materialAbout = sliderMaterial.querySelector('.journal-projito__about');
+  const mateirialQuote = sliderMaterial.querySelector('.journal-projito__text-quote');
+  materialTitle.textContent = item.title;
+  materialSubtitle.textContent = item.subtitle;
+  materialAbout.textContent = item.about;
+  mateirialQuote.textContent = item.quote;
+  sliderMaterial.style.backgroundImage = `url('${item.image}')`;
+  return sliderMaterial;
+
+}
+
+function renderNewsPrepend(cardNews,container) {
+  container.prepend(cardNews);
+}
+
+function checkRenderLimit(index,limit) {
+  if(index > limit - 1) {
+    return false;
+  }
+  return true;
 }
 
 closeButton.addEventListener("click", () => {
@@ -39,9 +122,33 @@ closeButton.addEventListener("click", () => {
 });
 
 
-window.addEventListener('resize', function(event){
-  replacedButtonNews();
+sectionNewsMain.forEach( item => {
+  const parentItem = item.parentNode;
+  replacedButtonNews(item,parentItem);
+  window.addEventListener('resize', () => {
+  replacedButtonNews(item,parentItem);
+});
+})
+
+sectionReaderAll.forEach ( item => {
+  item.querySelectorAll('.journal-projito__text-quote').forEach( item => {
+    item.style.display = 'block';
+  })
+})
+
+newsElement.forEach( (item,index) => {
+  const widthSection = checkWidthElement();
+  const limitRender = widthSection > 767 ? checkRenderLimit(index,limitIndex) : checkRenderLimit(index,limitMobile);
+  if(limitRender) {
+    const newCard = createNewsCard(item);
+    renderNewsPrepend(newCard,sliderNewsContainer);
+  }
 });
 
-replacedButtonNews();
-
+materialsElement.forEach( (item,index) => {
+  const limitRender = checkRenderLimit(index,limitIndex);
+  if(limitRender) {
+    const newCard = createMaterialsCard(item);
+    renderNewsPrepend(newCard,sliderJournalContainer);
+  }
+});
