a394ed405ba8791c659326ed3a813e6456a00d64
Danya Pavlov <fRo0neeee@gmail.com>
2022-07-21 18:21:23 +0300

    feat: fixed

--- scripts/header.js
+++ scripts/header.js
@@ -4,9 +4,7 @@ const header = document.querySelector('.header');
 checkbox.addEventListener('click', function() {
   if (checkbox.checked) {
 		header.classList.add('header_mobile-menu');
-    header.classList.remove('header_theme_white');
     return;
 	}
 	header.classList.remove('header_mobile-menu');
-  header.classList.add('header_theme_white')
 });
