2887e4d153b66eb0ab466b2ffcd2965a35913113
OneOfHerSisters <whatsshesaid@gmail.com>
2022-07-21 17:39:01 +0400

    fix: header background

--- scripts/header.js
+++ scripts/header.js
@@ -4,7 +4,9 @@ const header = document.querySelector('.header');
 checkbox.addEventListener('click', function() {
   if (checkbox.checked) {
 		header.classList.add('header_mobile-menu');
+    header.classList.remove('header_theme_white');
     return;
 	}
 	header.classList.remove('header_mobile-menu');
+  header.classList.add('header_theme_white')
 });
